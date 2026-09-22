b950a11bfe751c98a3fa6e7d447a9794531a504a
Dano Hawkins <d.d.a.hawkins@tudelft.nl>
2026-09-22 14:25:15 +0200

    Silence TSAM verbosity

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -180,7 +180,7 @@ def run_tsa_case(
             "preserve_column_means",
             True,
         ),
-        verbose=True,
+        verbose=False,
     )
 
     # ------------------------------------------------------------------
